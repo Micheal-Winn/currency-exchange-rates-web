e4ec780259dd28d26b3b76676183acc78c27f8dd
thantzinwin <thant.zin.winnnn@gmail.com>
2023-09-10 23:31:05 +0630

    improve : change back to client sider fetching to ssr for data fetching

--- src/app/currency/page.tsx
+++ src/app/currency/page.tsx
@@ -1,26 +1,13 @@
-"use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ExchangePage from "@/components/ExchangePage";
 
-const CurrencyConverterPagee = () => {
-  const [data, setData] = useState<Array<ctyObjProps>>([]);
-  const[loading,setLoading] = useState<boolean>(false)
-  useEffect(() => {
-    setLoading(true)
-    const exchangeApi = async () => {
-      const exchanges = await axios.get(
-        `${process.env.CURRENCY_ENDPOINT!}/live?access_key=${process.env.CURRENCY_KEY!}`
-      );
-      const data = exchanges.data;
-      for (const [key, value] of Object.entries(data.quotes!)) {
-        console.log(`${key}: ${value}`);
-        setData((prev) => [...prev, { name: key, value: value as number }]);
-      }
-      setLoading(false)
-    };
-    exchangeApi();
-  }, []);
+const CurrencyConverterPagee = async () => {
+  //api runs in SSR and give it to client not to be faced loading
+  const exchanges = await fetch(
+    `${process.env.CURRENCY_ENDPOINT}/live?access_key=${process.env.CURRENCY_KEY}`
+  );
+  const data = await exchanges.json();
 
   return (
     <main className="container pt-6">
@@ -28,7 +15,7 @@ const CurrencyConverterPagee = () => {
         Currency Exchange
       </h2>
 
-      {loading ? <p className="text-center">Loading...</p> : <ExchangePage quotes={data} />}
+      <ExchangePage quotes={data.quotes} />
     </main>
   );
 };
